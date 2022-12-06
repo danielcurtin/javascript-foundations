99f16c72f2ce338adef6424434ae3db8fb202feb
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-05 18:11:06 -0700

    Complete dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+    constructor(name, rider) {
+        var fed = 0;
+        this.name = name;
+        this.rider = rider;
+        this.greet = function () {
+            return `Hi, ${rider}!`;
+        };
+        this.hungry = true;
+        this.eat = function() { 
+            if (fed === 2) {
+                this.hungry = false;
+            } else {
+                fed = fed + 1;
+            };
+        };
+    };
+};
 
+module.exports = Dragon;
