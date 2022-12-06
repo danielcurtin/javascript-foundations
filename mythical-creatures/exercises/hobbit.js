00a332e1072d7491a3fadc462043baa135b0ed23
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-05 18:49:57 -0700

    Complete hobbit

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,29 @@
+class Hobbit {
+    constructor(name) {
+        this.name = name.name;
+        this.age = 0;
+        this.adult = false;
+        this.old = false;
+        this.hasRing = false;
+        this.celebrateBirthday = function() {
+            this.age = this.age + 1;
+            if (this.age < 33) {
+                this.adult = false;
+            } else if (this.age > 100) {
+                this.old = true;
+            } else {
+                this.adult = true;
+            }
+        };
+        this.getRing = function() {
+            if (this.name === "Frodo") {
+                this.hasRing = true;
+                return "Here is the ring!";
+            } else {
+                return "You can't have it!";
+            };
+        };
+    };
+};
+
+module.exports = Hobbit;
