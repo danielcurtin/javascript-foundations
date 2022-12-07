53aab1788dc55485c39af84747934069a06b1f41
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-06 22:38:00 -0700

    Complete medusa

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,23 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    };
+
+    gazeAtVictim(vic) {
+        var newStat = new Statue(vic.name);
+        if (this.statues.length < 3) {
+            this.statues.push(newStat);
+        } else if (this.statues.length === 3) {
+            var release = this.statues[0];
+            this.statues.shift();
+            this.statues.push(newStat);
+            return new Person(release.name, "relieved");
+        };
+    };
+};
+
+module.exports = Medusa;
