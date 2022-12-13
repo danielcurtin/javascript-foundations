b1a3c4b0c0f4f197aeb0c0451084ee419b4b0369
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-12 18:06:09 -0700

    Complete roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,25 @@
 class Roadrace {
+    constructor(raceOb) {
+        this.name = raceOb.title;
+        this.location = raceOb.city;
+        this.distance = 0;
+        this.participants = [];
+    };
 
-}
+    setDistance(miles) {
+        this.distance = miles;
+        return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+    };
+
+    registerParticipants(runner) {
+        this.participants.push(runner);
+    };
+
+    completeRace() {
+        for (var i = 0; i < this.participants.length; i++) {
+            this.participants[i].runRace(this.name, this.distance);
+        };
+    };
+};
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,26 @@
 class Runner {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    };
 
-}
+    runSomeMiles(milesRan) {
+        this.milesRun += milesRan;
+        this.fitness += milesRan;
+    };
+
+    stretch() {
+        this.fitness += .5;
+    };
+
+    runRace(name, length) {
+        this.completedRaces.push(name);
+        this.milesRun += length;
+        this.fitness += length;
+    };
+};
 
 module.exports = Runner;
