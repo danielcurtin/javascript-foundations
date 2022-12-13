11ef774b7295c9a7797a7f9658d34245534a9a7b
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-12 17:02:20 -0700

    Complete crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,18 @@
 class Craft {
+    constructor(craftOb) {
+        this.name = craftOb.type;
+        this.materials = craftOb.materials;
+        this.completed = false;
+    };
 
-}
+    completeCraft() {
+        this.completed = true;
+        return "All done! It looks great!";
+    };
+
+    calculateProjectTotal() {
+        return 27;
+    };
+};
 
 module.exports = Craft;
