3871e28c99d56e947ff39c1d4012ec4512631a09
Daniel Curtin <dcurtin.work@gmail.com>
2022-12-30 14:48:35 -0700

    Complete golf course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,25 @@
 class GolfCourse {
+    constructor(name, diff, numOpen, arrFeats) {
+        this.name = name;
+        this.difficulty = diff;
+        this.openings = numOpen;
+        this.features = arrFeats;
+        this.currentlyPlaying = [];
+    };
 
+    checkInGroup(group) {
+        if (group.length <= this.openings) {
+            this.openings -= group.length;
 
-}
+            for (var i = 0; i < group.length; i++) {
+                this.currentlyPlaying.unshift(group[i].name);
+            };
+
+            return "You're checked in. Have fun!";
+        } else {
+            return "Sorry, we are currently booked! Please come back later.";
+        };
+    };
+};
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,43 @@
 class Golfer {
+    constructor(golferOb) {
+        this.name = golferOb.name;
+        this.handicap = golferOb.handicap;
+        this.frustration = 0;
+        this.confidence = 0;
+    };
 
+    calculateAvg(par) {
+        return `I usually shoot a ${this.handicap + par} on average.`;
+    };
 
-}
+    playRound(course) {
+        if (course.difficulty === "moderate") {
+            this.frustration += 100;
+        } else {
+            this.frustration += 500;
+        };
+    };
+
+    hitTheRange() {
+        this.confidence += 10;
+    };
+
+    marvel(course) {
+        return `I love the ${course.features[0]} and ${course.features[1]} on this course!`;
+    };
+
+    whatYaShoot(num) {
+        if (num < 0) {
+            this.frustration = 0;
+            return 'I AM THE GREATEST GOLFER ALIVE!';
+        } else if (!num) {
+            this.frustration -= 10;
+            return 'Booyah!';
+        } else {
+            this.frustration += 20;
+            return 'Doh!';
+        };
+    };
+};
 
 module.exports = Golfer;
