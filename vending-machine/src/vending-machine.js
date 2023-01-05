dbeae9a8c9c0a0f2c749602ec90ec9faf8ecfac3
Daniel Curtin <dcurtin.work@gmail.com>
2023-01-05 11:05:14 -0700

    Complete vending machine

--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,31 @@
 class VendingMachine {
+    constructor(machineObj) {
+        this.id = machineObj.id;
+        this.isBroken = machineObj.isBroken;
+        this.snacks = [];
+    };
 
-}
+    addSnacks(snack) {
+        for (var i = 0; i < this.snacks.length; i++) {
+            if (this.snacks[i].name === snack.name) {
+                return "Sorry, that snack is already stocked! Try adding a different snack.";
+            };
+        };
+        this.snacks.push(snack);
+    };
+
+    purchaseSnack(snackName, money) {
+        for (var i = 0; i < this.snacks.length; i++) {
+            if (this.snacks[i].name === snackName && this.snacks[i].itemsInStock && money > this.snacks[i].price) {
+                this.snacks[i].removeItem();
+                return `Success! Here is $${money - this.snacks[i].price} back!`;
+            } else if (!this.snacks[i].itemsInStock) {
+                return "Sorry, no items in stock. Try another item.";
+            } else if (money < this.snacks[i].price) {
+                return "Sorry, not enough payment. Please add more money.";
+            };
+        };
+    };
+};
 
 module.exports = VendingMachine;
