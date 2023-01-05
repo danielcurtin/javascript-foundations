641ed88926f89d76ff1a1ba0bc58da83253d3f26
Daniel Curtin <dcurtin.work@gmail.com>
2023-01-05 11:51:52 -0700

    Complete snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,19 @@
 class Snowman {
+    constructor(detailsObj) {
+        this.carrots = detailsObj.carrots;
+        this.coal = detailsObj.coal;
+        this.buttons = detailsObj.buttons;
+        this.snowballs = detailsObj.snowballs;
+        this.magicHat = false;
+    };
 
-}
+    canWearMagicHat() {
+        if (this.coal > 1 && this.buttons > 4 && this.carrots > 0 && this.snowballs > 1) {
+            this.magicHat = true;
+            return true;
+        };
+        return false;
+    };
+};
 
 module.exports = Snowman;
