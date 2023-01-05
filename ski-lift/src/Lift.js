f71092ed04b302391a1e162e8c8e7030384a72a6
Daniel Curtin <dcurtin.work@gmail.com>
2023-01-05 11:37:22 -0700

    Complete ski lift

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,33 @@
 var Skier = require('./Skier');
 
 class Lift {
-  
-}
+    constructor(limit) {
+        this.inService = true;
+        this.limit = limit;
+        this.skiers = [];
+        this.safetyBar = "up";
+    };
+
+    admitSkier(name, ticket) {
+        if (this.skiers.length < this.limit && ticket) {
+            this.skiers.push(new Skier(name, ticket));
+        } else if (!ticket) {
+            return `Sorry, ${name}. You need a lift ticket!`;
+        } else {
+            return `Sorry, ${name}. Please wait for the next lift!`;
+        };
+    };
+
+    startLift() {
+        var space = this.limit - this.skiers.length;
+        if (!space) {
+            this.safetyBar = "down";
+        } else if (space > 1) {
+            return `We still need ${space} more skiers!`;
+        } else {
+            return `We still need ${space} more skier!`;
+        };
+    };
+};
 
 module.exports = Lift;
